dbd59f1f96f153a581009a83055ae97656a7455a
Max <maximumsomething@gmail.com>
2018-01-18 18:39:44 -0500

    PID control for driving straight

--- src/Subsystems/Drivetrain.h
+++ src/Subsystems/Drivetrain.h
@@ -6,20 +6,37 @@
 #include <vector>
 #include "RobotMap.h"
 
+
+struct DoubleMotor : PIDOutput {
+	virtual ~DoubleMotor() {};
+	SpeedController* motor1;
+	SpeedController* motor2;
+	
+	double outputMultiplier = 0;
+	
+	
+	void PIDWrite(double output) override;
+	DoubleMotor(SpeedController* motor1, SpeedController* motor2): motor1(motor1), motor2(motor2) {};
+};
+
 class Drivetrain : public frc::Subsystem {
 private:
 
 	double encoderOffset;
 
-	SpeedController *FLMotor;
-	SpeedController *BLMotor;
-	SpeedController *FRMotor;
-	SpeedController *BRMotor;
+	SpeedController* FLMotor = new frc::Victor(FLMotorChannel);
+	SpeedController* BLMotor = new frc::Victor(BLMotorChannel);
+	SpeedController* FRMotor = new frc::Victor(FRMotorChannel);
+	SpeedController* BRMotor = new frc::Victor(BRMotorChannel);
+	
+	DoubleMotor LeftMotors = DoubleMotor(FLMotor, BLMotor);
+	DoubleMotor RightMotors = DoubleMotor(FRMotor, BRMotor);
 
-	Encoder *FLEncoder;
-	Encoder *BLEncoder;
-	Encoder *FREncoder;
-	Encoder *BREncoder;
+	Encoder *LeftEncoder= new frc::Encoder(LeftEncoderChannel[0],LeftEncoderChannel[1]);
+	Encoder *RightEncoder = new frc::Encoder(RightEncoderChannel[0],RightEncoderChannel[1]);
+	
+	PIDController* LeftControl;
+	PIDController* RightControl;
 
 	void SetMotors(double FL, double BL, double FR, double BR);
 	void StopMotors();
@@ -29,5 +46,7 @@ public:
 	void InitDefaultCommand();
 	void Drive(double left, double right);			//Drives left and right wheels accordingly
 };
+
+
 
 #endif  // Drivetrain_H
