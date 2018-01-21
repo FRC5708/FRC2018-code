97b94e3933fff067968f32e25e3697c9fa71177e
Team 5708 Zebrotics <Team 5708 Zebrotics@10.113.56.147>
2018-01-21 12:56:20 -0500

    Fixed encoder stuff

--- src/Subsystems/Drivetrain.cpp
+++ src/Subsystems/Drivetrain.cpp
@@ -1,6 +1,9 @@
 #include "Drivetrain.h"
 #include "../Robot.h"
 #include <SmartDashboard/SmartDashboard.h>
+#include <networktables/NetworkTable.h>
+#include <iostream>
+#include <math.h>
 
 Drivetrain::Drivetrain() : frc::Subsystem("Drivetrain"), 
 leftSource(leftEncoder), 
@@ -8,12 +11,12 @@ rightSource(rightEncoder),
 leftOutput(FLMotor, BLMotor),
 rightOutput(FRMotor, BRMotor) {
 	
-	encoderOffset = 0;
+	encoderOffset = 0.0;
 	leftControl.Enable();
 	rightControl.Enable();
 	
-	leftEncoder->SetDistancePerPulse(1/360);
-	rightEncoder->SetDistancePerPulse(1/360);
+	leftEncoder->SetDistancePerPulse(1.0/360.0);
+	rightEncoder->SetDistancePerPulse(1.0/360.0);
 }
 
 double Drivetrain::Limit(double number) {
@@ -27,8 +30,13 @@ double Drivetrain::Limit(double number) {
 }
 
 void Drivetrain::Drive(double left, double right){
-	leftControl.SetSetpoint(left);
-	rightControl.SetSetpoint(right);
+	//leftControl.SetSetpoint(left);
+	//rightControl.SetSetpoint(right);
+
+	FLMotor->Set(left);
+	BLMotor->Set(left);
+	FRMotor->Set(right);
+	BRMotor->Set(right);
 }
 
 void Drivetrain::DrivePolar(double moveValue, double rotateValue) {
@@ -69,16 +77,28 @@ double Drivetrain::GetDistance(){
 	return ((leftEncoder->Get()+rightEncoder->Get())/2)*WheelCircumference;
 }
 
+double maxEncoderDistance = 0;
+double maxEncoderRate = 0;
+
 double Drivetrain::RatePIDSource::PIDGet() {
 	double toReturn = encoder->GetRate();
-	SmartDashboard::PutNumber("Encoder", toReturn);
+	double distance = encoder->GetDistance();
+	//SmartDashboard::PutNumber("Encoder", toReturn);
+	NetworkTable::GetTable("datatable")->PutNumber("encoder", toReturn);
+	NetworkTable::GetTable("datatable")->PutNumber("encoder", distance);
+
+
+	if (abs(toReturn > abs(maxEncoderRate))) maxEncoderRate = toReturn;
+	if (abs(distance > abs(maxEncoderDistance))) maxEncoderDistance = distance;
+
+	std::cout << "encoder: rate: " << toReturn << ", distance: " << distance << ", max rate: " << maxEncoderRate << ", max distance" << maxEncoderDistance << std::endl;
 	return toReturn;
 }
 
 
 void Drivetrain::DoubleMotorPIDOutput::PIDWrite(double d) {
-	motor1->Set(d);
-	motor2->Set(d);
+	//motor1->Set(d);
+	//motor2->Set(d);
 }
 
 
