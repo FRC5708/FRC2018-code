c48341c2b652c70c4a6adf522e470105efdc5a7b
max <maximumsomething@gmail.com>
2018-02-26 19:11:49 -0500

    Alternative control scheme for arm

--- src/Commands/ArmWithJoystick.cpp
+++ src/Commands/ArmWithJoystick.cpp
@@ -33,27 +33,51 @@ void ArmWithJoystick::Execute() {
 			wrist_is_up = true;
 		}
 	}
-	if (Robot::joystick->GetRawButtonPressed(1)) {
-		holding_arm = !holding_arm;
-		std::cout << "set holding_arm to: " << holding_arm << std::endl;
-	}
-	double armDir = ((Robot::joystick->GetRawButton(5) ? 1 : 0) + (Robot::joystick->GetRawButton(6) ? -1 : 0));
 	
-	if (armDir != 0) {
-		Robot::arm.CancelMoveTo();
+	if (Robot::joyMode == SINGLE_JOY) {
+		double armDir = ((Robot::joystick->GetRawButton(5) ? 1 : 0) + (Robot::joystick->GetRawButton(6) ? -1 : 0));
+		
+		if (Robot::joystick->GetRawButtonPressed(1)) {
+			holding_arm = !holding_arm;
+			std::cout << "set holding_arm to: " << holding_arm << std::endl;
+		}
+		if (armDir != 0) {
+			Robot::arm.CancelMoveTo();
 
-        if(fabs(arm_power) < 1){
-            arm_power+=.04*armDir; //Ramup/Rampdown
-        }
-		Robot::arm.Move(arm_power);
+			if(fabs(arm_power) < 1){
+				arm_power+=.04*armDir; //Ramup/Rampdown
+			}
+			Robot::arm.Move(arm_power);
+		}
+		else {
+			arm_power = 0;
+			if (holding_arm && !Robot::arm.isHolding) {
+				Robot::arm.StartHold();
+			}
+			else if (!holding_arm) {
+				Robot::arm.CancelMoveTo();
+			}
+		}
 	}
 	else {
-		arm_power = 0;
-		if (holding_arm && !Robot::arm.isHolding) {
-			Robot::arm.StartHold();
+		// right joystick button
+		if(Robot::joystick->GetRawButton(10)) {
+			if (!Robot::arm.isHolding) {
+				Robot::arm.StartHold();
+			}
 		}
-		else if (!holding_arm) {
+		else {
 			Robot::arm.CancelMoveTo();
+			
+			// should be right joystick X
+			double rawPower = Robot::joystick->GetRawAxis(4);
+			
+			// "dead zone" correction
+			double correctedPower = (abs(rawPower) - 0.1) * (1/1.1);
+			if (correctedPower < 0) correctedPower = 0;
+			if (rawPower < 0) correctedPower = -correctedPower;
+			
+			Robot::arm.Move(correctedPower);
 		}
 	}
 }
