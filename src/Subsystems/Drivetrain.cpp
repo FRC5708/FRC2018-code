09b4cd84a4881fc78b9649bc2029f3e0c44e47ff
max <maximumsomething@gmail.com>
2018-03-20 19:36:45 -0400

    Drivetrain rampup

--- src/Subsystems/Drivetrain.cpp
+++ src/Subsystems/Drivetrain.cpp
@@ -70,10 +70,10 @@ void Drivetrain::ResetDistance(){
 }
 
 double Drivetrain::GetDistance() {
-	return ((leftEncoder->GetDistance()+rightEncoder->GetDistance())/2.0)*WheelCircumference;
+	return (leftEncoder->GetDistance()/*+rightEncoder->GetDistance())/2.0*/)*WheelCircumference;
 }
 double Drivetrain::GetRate() {
-	return ((leftEncoder->GetRate()+rightEncoder->GetRate())/2.0)*WheelCircumference;
+	return (leftEncoder->GetRate()/*+rightEncoder->GetRate())/2.0*/)*WheelCircumference;
 }
 
 
