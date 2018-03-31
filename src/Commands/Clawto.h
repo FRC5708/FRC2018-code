5a11c3b521c8dd7b5e171cf62e5f0b3128f1e438
Team 5708 <Team 5708@10.10.48.32>
2018-03-31 19:25:29 -0400

    Andrew's (probably) futile attempts at making autonomous work.
    -Added Clawto class for auto claw, as well as WinchAuto
    -Modified autonomous slightly

--- src/Commands/Clawto.h
+++ src/Commands/Clawto.h
@@ -9,13 +9,15 @@
 
 class Clawto : public frc::Command {
 public:
-	double to;
-	Clawto();
+	Clawto(){};
 	void Initialize() override {
 	Robot::claw.MoveMotors(1.0);
 	std::this_thread::sleep_for(std::chrono::milliseconds(500));
 	Robot::claw.MoveMotors(0.0);
 	}
+	bool IsFinished() override{
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		return true;}
 };
 
 
